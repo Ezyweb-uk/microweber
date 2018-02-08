8baa7f32babbce39be0aab7ed9c26aa5d0f2320f
nickwuk <nick@estores.org.uk>
2018-02-08 18:24:54 +0000

    Optional video thumbnail image added as placeholder until clicked to reduce page size for SEO. Additional date formats added Twitter url corrected in Liteness template

--- userfiles/modules/video/templates/default.php
+++ userfiles/modules/video/templates/default.php
@@ -2,20 +2,19 @@
 if ($prior != '2' or $prior == false) {
     if ($code != '') {
         $code = trim($code);
-        if (stristr($code, '<iframe') !== false) {
-            $code = preg_replace('#\<iframe(.*?)\ssrc\=\"(.*?)\"(.*?)\>#i',
-                '<iframe$1 src="$2?wmode=transparent"$3>', $code);
-            //print '<div class="mwembed">' . $code . '</div>';
-        }
 
+		if (stristr($code, '<iframe') !== false) {
+			$code = preg_replace('#\<iframe(.*?)\ssrc\=\"(.*?)\"(.*?)\>#i', '<iframe$1 src="$2?wmode=transparent"$3>', $code);
+		}
 
-        if (video_module_is_embed($code) == true) {
-            print '<div class="mwembed">' . $code . '</div>';
-        } else {
-            print video_module_url2embed($code, $w, $h, $autoplay);
-        }
+		if (video_module_is_embed($code) == true) {
+			$code = '<div class="mwembed">' . $code . '</div>';
+		} else {
+			$code = video_module_url2embed($code, $w, $h, $autoplay);
+		}
+
     } else {
-        print lnotif("<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>", true);
+    	$show_video_settings_btn = true;
     }
 } else if ($prior == '2') {
     if ($upload != '') {
@@ -24,12 +23,23 @@ if ($prior != '2' or $prior == false) {
         } else {
             $autoplay = 'autoplay';
         }
-        print '<div class="mwembed "><video controls width="' . $w . '" height="' . $h . '" ' . $autoplay . ' src="' . $upload . '"></video></div>';
+        $code = '<div class="mwembed "><video controls width="' . $w . '" height="' . $h . '" ' . $autoplay . ' src="' . $upload . '"></video></div>';
     } else {
-        print ("<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>");
-
+        $show_video_settings_btn = true;
     }
 } else {
-    print "<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>";
+	$show_video_settings_btn = true;
 }
+
+if($show_video_settings_btn) {
+	$code = "<div class='video-module-default-view mw-open-module-settings'><img src='" . $config['url_to_module'] . "video.png' /></div>";
+} else {
+	if($use_thumbnail) {
+		$unique_id = str_replace('-','',$params['id']);
+		$css = '<style>.video-player{background: #000;}.video-player img:hover{cursor: pointer;}</style>' . "\n";
+		$script = '<script>function replaceImg' . $unique_id . '(img){var div = document.createElement("div");div.innerHTML = \'' . $code . '\';img.parentNode.replaceChild(div, img);}</script>' . "\n";
+		$code = $css . $script . '<div class="video-player"><img onclick="javascript:replaceImg' . $unique_id . '(this);" src="' . $thumb . '"></div>';
+	}
+}
+print $code;
 ?>
